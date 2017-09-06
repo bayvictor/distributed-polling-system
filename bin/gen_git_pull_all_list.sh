31cbd674bf8ca4531e60dc3f2fdda44aae4d3531
Victor Huang <bayvictor@gmail.com>
2017-09-06 00:46:34 +0000

    more docker scripts etc

--- bin/gen_git_pull_all_list.sh
+++ bin/gen_git_pull_all_list.sh
@@ -1,2 +1,4 @@
- tree -d -L 1|cut -d" " -f2|sed '1d;$d'|sed '$d'|sed 's/^/cd /g;s/$/;git pull;cd ../g'
+echo "unset CUR;export CUR=$PWD" 
+ find . |grep -e "\.git$"|sed 's/\.git$//g' | cut -d" " -f2|sed '1d;$d'|sed '$d'|sed 's/^/cd /g;s/$/;git pull;cd \$CUR;/g'
+chmod +x *.sh
 
