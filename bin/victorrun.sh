3209562243506f750c81aa06518fd9d91a58232d
Victor Huang <bayvictor@gmail.com>
2017-09-06 03:01:19 +0000

    shells

---
+++ bin/victorrun.sh
@@ -0,0 +1,8 @@
+# docker run -ti --rm --volume="$(pwd)":/bot zixia/wechaty mybot.js # for JavaScript
+#docker run wechaty:latest 
+
+ #docker run  -v $(pwd):/bot ./example/the-worlds-shortest-chatbot-code-in-6-lines.js -it wechaty:latest /bin/bash
+ docker run  -v $(pwd):/bot  -it wechatty:latest  ./example/the-worlds-shortest-chatbot-code-in-6-lines.js 
+ 
+
+
